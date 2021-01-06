e7c4a1a4b62a010f328213b5488df89f24f4b47c
ben <007vasy@gmail.com>
2021-01-06 12:43:06 +1100

    test ping to anzo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use reqwest;
+use reqwest::header::{HeaderMap, CONTENT_TYPE, AUTHORIZATION,HeaderValue};
+
+const ANZO_URL:String = "172.17.0.2".to_string();
+const ANZO_USER:String = "admin".to_string();
+const ANZO_PASS:String = "Passw0rd1".to_string();
+
+fn send_query_to_db(query:String) {
+    
+
+    let mut headers = HeaderMap::new();
+    headers.insert(AUTHORIZATION, HeaderValue::from_str(["Basic ",ANZO_USER,":",ANZO_PASS].concat().to_string()));
+    headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/sparql-query; charset=utf-8"));
+
+    let client = reqwest::blocking::Client::new();
+    let res = client    
+    .post(&ANZO_URL)
+    .headers(headers)
+    .body(query)
+    .send();
+    println!("{:?}", res);
+    ()
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Testing Payload To Anzo");
+
+    let query_init = "LOAD WITH 'global' <s3://csi-notebook-datasets/MovieTicketAnalysis/20190217/tickit.ttl.gz>
+    INTO GRAPH <tickit>".to_string();
+    
+    let query_test = "SELECT (count(*) as ?number_of_triples)
+    FROM <tickit>
+    WHERE { ?s ?p ?o }".to_string();
+
+    send_query_to_db(query_init);
+
+    send_query_to_db(query_test);
 }
